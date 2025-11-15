208a41a6fef55048a750b57eec0bdc9efc066c5f
appositum <appositum@pm.me>
2025-11-15 20:15:54 -0300

    no relative numbers on insert mode

--- init.lua
+++ init.lua
@@ -240,6 +240,15 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- set relative number lines on normal mode but not insert mode
+vim.api.nvim_create_autocmd({ 'InsertEnter' }, {
+  command = 'set number norelativenumber',
+})
+
+vim.api.nvim_create_autocmd({ 'InsertLeave' }, {
+  command = 'set number relativenumber',
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
