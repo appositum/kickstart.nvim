9ca98b97eaef6d26839c1224917ce0db01bfbd57
appositum <appositum@pm.me>
2025-12-15 01:02:27 -0300

    change `format` keybind (was conflicting with fuzzy finder keybinds)

--- init.lua
+++ init.lua
@@ -817,7 +817,7 @@ require('lazy').setup({
     cmd = { 'ConformInfo' },
     keys = {
       {
-        '<leader>f',
+        '<leader>fm',
         function()
           require('conform').format { async = true, lsp_format = 'fallback' }
         end,
