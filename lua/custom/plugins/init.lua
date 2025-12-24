73f7e934d26951ea52fa97d2b97f70c77188d07e
appositum <appositum@pm.me>
2025-12-24 20:35:54 -0300

    add octo.nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -158,5 +158,51 @@ return {
   },
   {
     'rhysd/git-messenger.vim',
-  }
+  },
+  {
+    "pwntester/octo.nvim",
+    cmd = "Octo",
+    opts = {
+      -- or "fzf-lua" or "snacks" or "default"
+      picker = "telescope",
+      -- bare Octo command opens picker of commands
+      enable_builtin = true,
+    },
+    keys = {
+      {
+        "<leader>gi",
+        "<CMD>Octo issue list<CR>",
+        desc = "List GitHub Issues",
+      },
+      {
+        "<leader>gp",
+        "<CMD>Octo pr list<CR>",
+        desc = "List GitHub PullRequests",
+      },
+      -- {
+      --   "<leader>gd",
+      --   "<CMD>Octo discussion list<CR>",
+      --   desc = "List GitHub Discussions",
+      -- },
+      {
+        "<leader>gn",
+        "<CMD>Octo notification list<CR>",
+        desc = "List GitHub Notifications",
+      },
+      {
+        "<leader>gs",
+        function()
+          require("octo.utils").create_base_search_command { include_current_repo = true }
+        end,
+        desc = "Search GitHub",
+      },
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      -- OR "ibhagwan/fzf-lua",
+      -- OR "folke/snacks.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+  },
 }
