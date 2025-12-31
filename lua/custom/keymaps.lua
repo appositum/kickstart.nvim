7324287a423f6ab9b82e39eb7dff280ea6b769df
appositum <appositum@pm.me>
2025-12-31 15:55:16 -0300

    move nvim custom keymaps to separate file

---
+++ lua/custom/keymaps.lua
@@ -0,0 +1,49 @@
+vim.keymap.set('n', 'k', 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', { desc = 'Move up through wrapped lines', expr = true })
+vim.keymap.set('n', 'j', 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', { desc = 'Move down through wrapped lines', expr = true })
+
+vim.keymap.set('n', 'x', '"_x', { desc = 'Delete without copying' })
+vim.keymap.set('n', '<leader>o', 'o<Esc>', { desc = 'Insert bottom newline' })
+vim.keymap.set('n', '<leader>O', 'O<Esc>', { desc = 'Insert top newline' })
+vim.keymap.set('n', '<leader>s', 'i<Space><Esc>', { desc = 'Insert whitespace on cursor' })
+vim.keymap.set('n', '<leader>S', 'a<Space><Esc>', { desc = 'Insert whitespace after cursor' })
+vim.keymap.set('n', '<C-S>', '<cmd>write<cr>', { desc = 'Save file' })
+vim.keymap.set('n', 'mj', '"zdd"zp', { desc = 'Move line down' })
+vim.keymap.set('n', 'mk', '"zddk"zP', { desc = 'Move line up' })
+vim.keymap.set('n', '<leader>a', ':keepjumps normal! ggVG<cr>', { desc = 'Select all' })
+vim.keymap.set('n', '<leader>r', ':reg<cr>', { desc = 'Open registers' })
+vim.keymap.set('n', '<leader>e', ':Neotree focus<cr>', { desc = 'Open neotree' })
+vim.keymap.set('n', '<leader>bd', '<cmd>bdelete<cr>', { desc = 'Delete current buffer' })
+vim.keymap.set('n', '<leader>bl', '<cmd>buffer #<cr>', { desc = 'Switch back to last buffer' })
+vim.keymap.set('n', '<leader>bn', '<cmd>bn<cr>', { desc = 'Next buffer' })
+vim.keymap.set('n', '<leader>bp', '<cmd>bp<cr>', { desc = 'Previous buffer' })
+vim.keymap.set('n', '<leader>x', ':w<cr><cmd>bdelete<cr>', { desc = 'Save and close buffer' })
+vim.keymap.set('n', "<leader>'", "cw'<Esc>pa'<Esc>", { desc = 'Wrap next word in single quotes' })
+vim.keymap.set('n', '<leader>"', 'cw"<Esc>pa"<Esc>', { desc = 'Wrap next word in double quotes' })
+
+vim.keymap.set('i', '<C-h>', '<Left>', { desc = 'Move left' })
+vim.keymap.set('i', '<C-l>', '<Right>', { desc = 'Move right' })
+vim.keymap.set('i', '<C-j>', '<Down>', { desc = 'Move down' })
+vim.keymap.set('i', '<C-k>', '<Up>', { desc = 'Move up' })
+vim.keymap.set('i', '<C-d>', '<Delete>', { desc = 'Delete in insert mode' })
+vim.keymap.set('i', '<C-x>', '<Backspace>', { desc = 'Backspace in insert mode' })
+
+vim.keymap.set('v', '<', '<gv', { desc = 'Indent line' })
+vim.keymap.set('v', '>', '>gv', { desc = 'Indent line' })
+
+vim.keymap.set('x', 'p', 'P', { desc = 'Paste without yanking in visual mode' })
+
+vim.keymap.set('n', '<leader>l', function()
+  vim.diagnostic.open_float()
+end)
+
+vim.keymap.set('n', 'L', function()
+  vim.diagnostic.config { virtual_lines = { current_line = true }, virtual_text = false }
+
+  vim.api.nvim_create_autocmd('CursorMoved', {
+    group = vim.api.nvim_create_augroup('line-diagnostics', { clear = true }),
+    callback = function()
+      vim.diagnostic.config { virtual_lines = false, virtual_text = true }
+      return true
+    end,
+  })
+end)
