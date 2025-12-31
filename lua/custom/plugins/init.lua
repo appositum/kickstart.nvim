9c9d55b25f1f46a3eb2f6b40596d770a40d61620
appositum <appositum@pm.me>
2025-12-31 15:25:37 -0300

    format

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -29,108 +29,108 @@ return {
       'nvim-tree/nvim-web-devicons',
     },
     config = function()
-      MODE_MAP =
-        {
-          ['NORMAL'] = 'N',
-          ['O-PENDING'] = 'N?',
-          ['INSERT'] = 'I',
-          ['VISUAL'] = 'V',
-          ['V-BLOCK'] = 'VB',
-          ['V-LINE'] = 'VL',
-          ['V-REPLACE'] = 'VR',
-          ['REPLACE'] = 'R',
-          ['COMMAND'] = '!',
-          ['SHELL'] = 'SH',
-          ['TERMINAL'] = 'T',
-          ['EX'] = 'X',
-          ['S-BLOCK'] = 'SB',
-          ['S-LINE'] = 'SL',
-          ['SELECT'] = 'S',
-          ['CONFIRM'] = 'Y?',
-          ['MORE'] = 'M',
-        }, require('lualine').setup {
-          options = {
-            theme = 'catppuccin_mocha',
-            icons_enabled = true,
-            -- component_separators = { left = '', right = '' },
-            -- section_separators = { left = '', right = '' },
+      MODE_MAP = {
+        ['NORMAL'] = 'N',
+        ['O-PENDING'] = 'N?',
+        ['INSERT'] = 'I',
+        ['VISUAL'] = 'V',
+        ['V-BLOCK'] = 'VB',
+        ['V-LINE'] = 'VL',
+        ['V-REPLACE'] = 'VR',
+        ['REPLACE'] = 'R',
+        ['COMMAND'] = '!',
+        ['SHELL'] = 'SH',
+        ['TERMINAL'] = 'T',
+        ['EX'] = 'X',
+        ['S-BLOCK'] = 'SB',
+        ['S-LINE'] = 'SL',
+        ['SELECT'] = 'S',
+        ['CONFIRM'] = 'Y?',
+        ['MORE'] = 'M',
+      }
+      require('lualine').setup {
+        options = {
+          theme = 'catppuccin_mocha',
+          icons_enabled = true,
+          -- component_separators = { left = '', right = '' },
+          -- section_separators = { left = '', right = '' },
 
-            -- component_separators = { left = '', right = '' },
-            -- section_separators = { left = '', right = '' },
+          -- component_separators = { left = '', right = '' },
+          -- section_separators = { left = '', right = '' },
 
-            -- component_separators = { left = '', right = '' },
-            -- section_separators = { left = '', right = '' },
+          -- component_separators = { left = '', right = '' },
+          -- section_separators = { left = '', right = '' },
 
-            component_separators = { left = '|', right = '|' },
-            section_separators = { left = '', right = '' },
+          component_separators = { left = '|', right = '|' },
+          section_separators = { left = '', right = '' },
+        },
+        sections = {
+          lualine_a = {
+            {
+              'mode',
+              fmt = function(s)
+                return ' ' .. (MODE_MAP[s] or s)
+              end,
+            },
           },
-          sections = {
-            lualine_a = {
-              {
-                'mode',
-                fmt = function(s)
-                  return ' ' .. (MODE_MAP[s] or s)
-                end,
+          lualine_b = {
+            {
+              'filetype',
+              icon_only = true,
+              colored = false,
+              separator = '',
+              padding = {
+                right = 0,
+                left = 1,
               },
             },
-            lualine_b = {
-              {
-                'filetype',
-                icon_only = true,
-                colored = false,
-                separator = '',
-                padding = {
-                  right = 0,
-                  left = 1,
-                },
-              },
-              'filename',
-              {
-                'diagnostics',
-                symbols = {
-                  error = ' ',
-                  warn = ' ',
-                  info = ' ',
-                },
+            'filename',
+            {
+              'diagnostics',
+              symbols = {
+                error = ' ',
+                warn = ' ',
+                info = ' ',
               },
             },
-            lualine_c = {
-              'branch',
-              {
-                'diff',
-                symbols = {
-                  added = ' ',
-                  -- modified = ' ',
-                  modified = ' ',
-                  removed = ' ',
-                },
+          },
+          lualine_c = {
+            'branch',
+            {
+              'diff',
+              symbols = {
+                added = ' ',
+                -- modified = ' ',
+                modified = ' ',
+                removed = ' ',
               },
             },
-            lualine_x = {
-              -- icon =
-              {
-                function()
-                  local msg = 'No Active Lsp'
-                  local buf_ft = vim.api.nvim_get_option_value('filetype', { buf = 0 })
-                  local clients = vim.lsp.get_clients()
-                  if next(clients) == nil then
-                    return msg
-                  end
-                  for _, client in ipairs(clients) do
-                    local filetypes = client.config.filetypes
-                    if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-                      return client.name
-                    end
-                  end
+          },
+          lualine_x = {
+            -- icon =
+            {
+              function()
+                local msg = 'No Active Lsp'
+                local buf_ft = vim.api.nvim_get_option_value('filetype', { buf = 0 })
+                local clients = vim.lsp.get_clients()
+                if next(clients) == nil then
                   return msg
-                end,
-                icon = ' LSP:',
-              },
+                end
+                for _, client in ipairs(clients) do
+                  local filetypes = client.config.filetypes
+                  if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                    return client.name
+                  end
+                end
+                return msg
+              end,
+              icon = ' LSP:',
             },
-            lualine_y = { 'progress' },
-            lualine_z = { 'location' },
           },
-        }
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' },
+        },
+      }
     end,
   },
   {
