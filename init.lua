53b37ff1ad695808b8f2374b80733ac45e6d0036
appositum <appositum@pm.me>
2025-12-31 15:31:28 -0300

    bring back old theme setup from old `init.lua` but disable tokyonight

--- init.lua
+++ init.lua
@@ -992,16 +992,20 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'catppuccin/nvim',
-    name = 'catppuccin',
+    'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
-      require('catppuccin').setup {
+      ---@diagnostic disable-next-line: missing-fields
+      require('tokyonight').setup {
         styles = {
-          comments = { 'nocombine' }, -- Disable italics in comments
+          comments = { italic = false }, -- Disable italics in comments
         },
       }
-      vim.cmd.colorscheme 'catppuccin'
+
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      -- vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
 
