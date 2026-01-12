2c289ed0e9e9ede79f809e8ad796b6059fe713ef
appositum <appositum@pm.me>
2026-01-12 08:50:07 -0300

    ensure mason language servers are installed
    - C
    - rust
    - python (+ formatting)
    - elixir
    - haskell (+ formatting)
    - typescript

--- init.lua
+++ init.lua
@@ -640,17 +640,19 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {},
         -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
+        pyright = {},
+        rust_analyzer = {},
+        elixir_ls = {},
+        hls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {},
         --
 
         lua_ls = {
@@ -685,6 +687,8 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'ruff', -- python format
+        'ormolu', -- haskell format
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
