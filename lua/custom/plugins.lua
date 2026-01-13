c4d2c1f93dd6b4c4bd057706235b2d9aaaae045e
appositum <appositum@pm.me>
2026-01-13 08:43:05 -0300

    make separators squared
    againnnnn

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -68,8 +68,8 @@ return {
           -- component_separators = { left = '', right = '' },
           -- section_separators = { left = '', right = '' },
 
-          component_separators = { left = '', right = '|' },
-          section_separators = { left = '', right = '' },
+          component_separators = { left = '|', right = '|' },
+          section_separators = { left = '', right = '' },
         },
         sections = {
           lualine_a = {
